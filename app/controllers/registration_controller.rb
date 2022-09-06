24f5a05a6469b418f2e69be8296af5a9b11e60b8
Usman879 <usmannoor519@gmail.com>
2022-09-06 11:23:55 +0500

    Country_select gem is working

--- app/controllers/registration_controller.rb
+++ app/controllers/registration_controller.rb
@@ -1,12 +1,13 @@
-class RegistrationController < Devise::RegistrationsController
-  before_action :configure_permitted_parameters, if: :devise_controller?
+class RegistrationController < Devise::RegistrationController
+  
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
  
-  protected
+protected
 
 def configure_permitted_parameters
-  devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :country, :phone_number, :email, :password) }
-  devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :country, :phone_number,:email, :password, :current_password) }
+  devise_parameter_sanitizer.permit(:sign_up).push { |u| u.permit(:first_name, :last_name, :country, :phone_number, :email, :password) }
+  devise_parameter_sanitizer.permit(:account_update).push { |u| u.permit(:first_name, :last_name, :country, :phone_number, :email, :password, :current_password) }
 end
 
 end
