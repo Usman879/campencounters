6da644f2dcf6d84b1bd0c44a6f6b2153da64b031
Usman879 <usmannoor519@gmail.com>
2022-09-08 17:42:10 +0500

    Users listing

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -3,4 +3,42 @@
 # Missing top-level documentation
 #
 class AdminController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+
+	def index
+    @users = User.all
+  end
+
+	def show
+	end
+
+	def edit
+	end
+	
+	def update
+		
+		if @user.update(user_params)
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	
+	end
+
+	def destroy
+		@user.destroy
+		redirect_to root_path
+	end
+
+
+	private
+
+	def find_user
+    @user = User.find(params[:id])
+  end
+
+	def user_params
+    params.require(:user).permit(:id, :first_name, :last_name, :email, :phone_number, :country, :type)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@
 
 Rails.application.routes.draw do
   root 'welcome#index'
+  resources :admin
   devise_for :users, controllers: { registrations: 'registration' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
