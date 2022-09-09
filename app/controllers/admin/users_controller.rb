6a83b063e3defef4440aecde74c9b373db98730f
Usman879 <usmannoor519@gmail.com>
2022-09-09 12:57:17 +0500

    User listing

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,21 @@
-# frozen_string_literal: true
-
-# Missing top-level documentation
-#
-class AdminController < ApplicationController
+class Admin::UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update, :destroy]
 
 	def index
     @users = User.all
+  end
+
+  def new
+    @user=User.new
+  end
+
+  def create
+    @user=User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render 'new'
+    end
   end
 
 	def show
@@ -16,9 +25,8 @@ class AdminController < ApplicationController
 	end
 	
 	def update
-		
 		if @user.update(user_params)
-			redirect_to @user
+			redirect_to admin_user_path
 		else
 			render 'edit'
 		end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 Rails.application.routes.draw do
   root 'welcome#index'
-  resources :admin
+  namespace :admin do
+    resources :users
+  end
   devise_for :users, controllers: { registrations: 'registration' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
