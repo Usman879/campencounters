580dd6c616d7941a544365abb472c07c297ac70b
Usman879 <usmannoor519@gmail.com>
2022-09-14 15:47:55 +0500

    Crud operations in camps along with searching, pagination

--- app/controllers/admin/camps_controller.rb
+++ app/controllers/admin/camps_controller.rb
@@ -1,15 +1,25 @@
 class Admin::CampsController < ApplicationController
 
   before_action :find_camp, only: [:show, :edit, :update, :destroy]
-
+  helper_method :sort_column, :sort_direction
+  
   def index
 
     if params[:search_key]
+    
       @camps = Camp.where("LOWER(title) LIKE ?" , 
-      "%#{params[:search_key]}%")
+      "%#{params[:search_key]}%").page(params[:page])
+    
     else
-      @camps = Camp.all
+      @camps = Camp.order(sort_column + " " + sort_direction)
+      @camps = Camp.order(:id).page(params[:page])
+      respond_to do |format|
+        format.html
+        format.csv { send_data @camps.to_csv }
+      end
+
     end
+
   end
 
   def new
@@ -17,7 +27,7 @@ class Admin::CampsController < ApplicationController
   end
 
   def create
-    @camp = Camp.new
+    @camp = Camp.new(camp_params)
     if @camp.save
       redirect_to admin_camps_path , notice: "Camp Successfully Created."
     else
@@ -49,14 +59,22 @@ class Admin::CampsController < ApplicationController
   def find_camp
     @camp = Camp.find(params[:id])
   end
-
+  
   def camp_params
-    params.require(:camp).permit(
-      :id, :title, :locations, :camp_type, :camp_status, 
-      :applicant_registration_start_date, :applicant_registration_start_time,
-      :applicant_registration_end_date, :applicant_registration_end_time
-      :parent_application_start_date, :parent_application_start_time,
-      :parent_application_end_date, :parent_application_end_time)
+    params.require(:camp).permit(:id, :title, :camp_type, :camp_status,
+     :applicant_registration_start_date, :applicant_registration_start_time,
+     :applicant_registration_end_date, :applicant_registration_end_time,
+     :parent_application_start_date, :parent_application_start_time,
+     :parent_application_end_date, :parent_application_end_time,
+     locations: []
+    )
+  end
+  def sort_column
+    Camp.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 
 end
--- app/models/camp.rb
+++ app/models/camp.rb
@@ -1,3 +1,19 @@
+require 'csv'
 class Camp < ApplicationRecord
-  enum camp_type: { virtual: 0, physical: 1, hybrid: 2 }
+
+  enum camp_type: { Virtual: 0, Physical: 1, Hybrid: 2 }
+  enum camp_status: { Inactive: 0, Active: 1 }
+
+  def self.to_csv
+    attributes = %w{ id title locations camp_status applicant_registration_start_date }
+
+    CSV.generate(headers: true ) do |csv|
+      csv << attributes
+
+      all.each do |camp|
+        csv << attributes.map {|attr| camp.send(attr)}
+      end
+    end
+  end
+
 end
